c64cb469f946dff9b9ceefa5f1c154eb5908b644
francisN21 <ininorones@gmail.com>
2026-01-23 23:42:55 -0800

    database is now aware of jest

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
 // src/db.js
 const { Pool } = require("pg");
 
+const isTest = process.env.NODE_ENV === "test";
+
 const pool = new Pool({
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT || 5432),
-  database: process.env.PGDATABASE,
+  database: isTest ? process.env.PGTESTDATABASE : process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
 });
