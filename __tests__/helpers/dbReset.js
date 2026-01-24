a413ca57aa3c438c29f193e1d3fc2cbb1a9caab9
francisN21 <ininorones@gmail.com>
2026-01-24 01:05:25 -0800

    Schemas are now created and working passing through Jest

--- __tests__/helpers/dbReset.js
+++ __tests__/helpers/dbReset.js
@@ -1,11 +1,9 @@
 const { pool } = require("../../src/db");
 
 async function resetDb() {
-  // Order matters because of FK constraints
-  await pool.query("TRUNCATE TABLE email_tokens RESTART IDENTITY CASCADE");
-  await pool.query("TRUNCATE TABLE sessions RESTART IDENTITY CASCADE");
-  await pool.query("TRUNCATE TABLE oauth_identities RESTART IDENTITY CASCADE");
-  await pool.query("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
+  await pool.query(
+    "TRUNCATE TABLE email_tokens, sessions, oauth_identities, users RESTART IDENTITY CASCADE"
+  );
 }
 
 module.exports = { resetDb };
